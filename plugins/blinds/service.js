212ee8b24f78b277f9b452c57c4f36e9c8dac2f9
claude-abounegm <claudenegm@gmail.com>
2017-04-23 17:47:12 -0400

    Added more blinds functionality

--- plugins/blinds/service.js
+++ plugins/blinds/service.js
@@ -1,31 +1,37 @@
 +function () {
-	'use strict';
+    'use strict';
 
     angular.module('SmartMirror').factory('BlindsService', function BlindsService($http) {
-        let service = {};
+        return {
+            sendCommandToBlind: function (state) {
+                let data = {};
+                data[config.commands.action] = config.commands[state];
 
-        //
-        service.sendCommandToBlind = function (state) {
-            console.log(state);
+                $http
+                    .put("http://" + config.blinds.ip, data)
+                    .then((data, status) => {
+                        console.log(status, data);
+                    }, (response) => {
+                        console.log('error ', response);
+                    });
+            }
         };
 
         /*function updateHue(i, index, setting) {
-            var update = {};
-            update["transitiontime"] = 10;
+         var update = {};
+         update["transitiontime"] = 10;
 
-            update['on'] = setting['on'];
-            if (setting['on']) {
-                update['hue'] = setting['colorHSV'][0];
-                update['sat'] = setting['colorHSV'][1];
-                update['bri'] = Math.round(setting['colorHSV'][2] * setting['brightness']);
-            }
+         update['on'] = setting['on'];
+         if (setting['on']) {
+         update['hue'] = setting['colorHSV'][0];
+         update['sat'] = setting['colorHSV'][1];
+         update['bri'] = Math.round(setting['colorHSV'][2] * setting['brightness']);
+         }
 
-            $http.put('http://' + config.light.settings.hueIp + '/api/' + config.light.settings.hueUsername + "/groups/" + config.light.setup[index].targets[i].id + "/action", update)
-                .success(function (data, status) {
-                    console.log(status, data);
-                })
-        }*/
-
-        return service;
+         $http.put('http://' + config.light.settings.hueIp + '/api/' + config.light.settings.hueUsername + "/groups/" + config.light.setup[index].targets[i].id + "/action", update)
+         .success(function (data, status) {
+         console.log(status, data);
+         })
+         }*/
     });
 }();
